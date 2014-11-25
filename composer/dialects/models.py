4d825f2a5fddeb474a323f8155f28ed4f04cd2b4
Cindy Cao <cindy.hy.cao@gmail.com>
2014-11-24 17:09:39 -0800

    Remove composer stuff since this is splitted from type-composer repo

--- composer/dialects/models.py
+++
@@ -1,6 +0,0 @@
-from bulbs.model import Node
-from bulbs.property import String, Integer, DateTime, Dictionary
-
-class CompositeNode(Node):
-    element_type = 'CompositeNode'
-    _serialized = String()
